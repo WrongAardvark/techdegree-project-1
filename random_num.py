fbbb954235c599ff19af2578b2eb452642c0f280
Mark Dowell <markd4296@gmail.com>
2021-08-19 00:55:11 -0500

    Final Version Complete
    Finally got it done.

--- random_num.py
+++ random_num.py
@@ -6,58 +6,61 @@ def start_game():
     guess_amount = 0
     play_game = True
     high_score = []
+    player_guess = None
     
     while play_game:
         random_num = random.randint(1,10)
         
         try:
-            player_guess = int(input("I picked a number between 1 and 10. Try to guess:  "))
-        except ValueError:
-            print("")
-            print("Make sure you use an integer, not a letter or word.")
-            continue
-        
-        while player_guess != random_num:
-         
-            try:
-                int(player_guess) == False
-            except ValueError:
-                print("")
-                player_guess = input("ValueError: Please use an integer:  ")
-            
-            if int(player_guess) > 10:
-                print("")
-                player_guess = input("Use a number that doesn't exceed the range:  ")
-            
-            guess_amount += 1
-            
-            if int(player_guess) < random_num:
-                print("")
-                player_guess = input("Your number was too low, try again:  ")
+            while player_guess != random_num:
                 
-            elif int(player_guess) > random_num:
-                print("")
-                player_guess = input("Your number was too high, try again:  ")            
-                      
-            elif int(player_guess) == random_num:
-                print("You got it! The number was {}!".format(random_num))
-                print("It took you {} guesses.".format(guess_amount))
-                high_score.append(guess_amount)
+                temp_player_guess = input("I picked a number between 1 and 10. Try to guess:  ")
+                player_guess = int(temp_player_guess)
+            
+                if player_guess > 10 or player_guess < 1:
+                    raise ValueError("Use a number that doesn't exceed the range:  ")
+                    continue
                 
-                keep_playing = input("Would you like to play again? (yes/no):  ")
-                
-                if keep_playing.lower() == "yes":
-                    guess_amount = 0
+                elif player_guess < random_num:
                     print("")
-                    print("{} is your current high score. Try to beat it!".format(min(high_score)))
-                    break
-                elif keep_playing.lower() != "yes":
-                    print("Your lowest number of guesses was {}.".format(min(high_score)))
-                    #looked up min() for high score. found on https://computersciencehub.io/python/python-find-smallest-number-in-list/
+                    print("Your number was too low, try again:  ")
+                    guess_amount += 1
+                    continue
                     
-                    print("Thank you for playing!")
-                    play_game = False
-                    break
-    
-# Kick off the program by calling the start_game function.
-start_game()                
+                elif player_guess > random_num:
+                    print("")
+                    print("Your number was too high, try again:  ") 
+                    guess_amount += 1
+                    continue
+                          
+                else:
+                    guess_amount += 1
+                    if player_guess == random_num:
+                        print("You got it! The number was {}!".format(random_num))
+                        print("It took you {} guesses.".format(guess_amount))
+                        high_score.append(guess_amount)
+                        break
+                    
+        except ValueError as ve:
+            if "invalid" in str(ve):
+                print("ValueError: Please use an integer:  ")
+                
+            else:
+                print("Out of range. Please try again.")
+            continue
+            
+        keep_playing = input("Would you like to play again? (yes/no):  ")
+        
+        if keep_playing.lower() == "yes":
+            guess_amount = 0
+            print("")
+            print("{} is your current high score. Try to beat it!".format(min(high_score)))
+            continue
+        elif keep_playing.lower() != "yes":
+            print("Your lowest number of guesses was {}.".format(min(high_score)))
+            
+            print("Thank you for playing!")
+            play_game = False
+            break
+
+start_game()  
